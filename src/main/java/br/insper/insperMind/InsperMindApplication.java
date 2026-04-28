8ac4646858b5957429d77e49a4884ae172bc57a3
hector-dream <hectorsoares2005@gmail.com>
2026-04-28 13:22:24 -0300

    Feat: spring initializr
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/br/insper/insperMind/InsperMindApplication.java
@@ -0,0 +1,13 @@
+package br.insper.insperMind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsperMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsperMindApplication.class, args);
+	}
+
+}
